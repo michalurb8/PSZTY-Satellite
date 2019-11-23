c7378279657943b922292f22dc4ec7056e924d9d
michalurb8 <michalurb8@wp.pl>
2019-11-23 16:47:19 +0100

    Add a lot, drawing class, simulation structure

--- src/Universe.cpp
+++ src/Universe.cpp
@@ -1,6 +1,28 @@
-#include "Universe.h"
 #include <fstream>
 #include <iostream>
+
+#include "Universe.h"
+
+Universe::Universe()
+{
+    //...constructor
+    LoadAssetsToDraw();
+}
+void Universe::AddPlanet(unsigned int angleArg, unsigned int radArg)
+{
+    planets.push_back(Planet(angleArg, radArg));
+}
+
+void Universe::Display()
+{
+    drawingObject.Draw();
+}
+
+void Universe::LoadAssetsToDraw()
+{
+    drawingObject.LoadPlanets(&planets);
+    //..rockets
+}
 
 void Universe::ShellResolve(char choice)
 {
@@ -13,8 +35,16 @@ void Universe::ShellResolve(char choice)
             std::cout << std::endl <<
             "Available commands:" << std::endl << std::endl <<
             "   h - Display this help message" << std::endl <<
+            "   d - Display generation" << std::endl <<
+            "   c - Close window" << std::endl <<
             "   q - Exit" << std::endl;
 			return;
+        case 'c':
+            drawingObject.CloseWindow();
+            break;
+        case 'd':
+            Display();
+            break;
         default:
             std::cout << "Unknown command. Try 'h' for help." << std::endl;
     }
--- src/Universe.h
+++ src/Universe.h
@@ -3,14 +3,23 @@
 
 #include <vector>
 #include <string>
+
 #include "Planet.h"
+#include "DrawingClass.h"
 
 class Universe
 {
     std::vector<Planet> planets;
+    DrawingClass drawingObject;
 public:
+    Universe();
+
     void ShellResolve(char choice);
     void ShellLoop();
+    void AddPlanet(unsigned int angleArg, unsigned int radArg);
+
+    void LoadAssetsToDraw();
+    void Display();
 };
 
 #endif
