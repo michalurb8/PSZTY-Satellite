9bd40cf52886598d6d6b3803ef089f9df792d355
michalurb8 <michalurb8@wp.pl>
2019-11-23 14:21:46 +0100

    Add planet, ready branches

---
+++ src/Planet.h
@@ -0,0 +1,11 @@
+#ifndef PLANET_H
+#define PLANET_H
+
+class Planet
+{
+    static unsigned int mass;
+    unsigned int xPos, yPos;    
+    unsigned int velocity;
+};
+
+#endif
--- src/Universe.h
+++ src/Universe.h
@@ -1,10 +1,13 @@
 #ifndef UNIVERSE_H
 #define UNIVERSE_H
 
+#include <vector>
 #include <string>
+#include "Planet.h"
 
 class Universe
 {
+    std::vector<Planet> planets;
 public:
     void ShellResolve(char choice);
     void ShellLoop();
